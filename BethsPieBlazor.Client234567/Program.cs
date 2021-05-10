4d96c7221ca93c9f63b2da5934b31a95c5f2b6ee
dabeat <dabeat1@hotmail.com>
2021-05-10 20:32:59 +0100

    6projects EmpyClient&Server sharing all codes from RCL

---
+++ BethsPieBlazor.Client234567/Program.cs
@@ -0,0 +1,35 @@
+using GSBlazor;
+using GSBlazor.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethsPieBlazor.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri("https://localhost:44340/"));
+            //builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(client => client.BaseAddress = new Uri("https://localhost:44340/"));
+            //builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(client => client.BaseAddress = new Uri("https://localhost:44340/"));
+            builder.Services.AddHttpClient<IEmployeeDataService, EmployeeDataService>(client => client.BaseAddress = new Uri(DataAccess.BaseLocation));
+            builder.Services.AddHttpClient<ICountryDataService, CountryDataService>(client => client.BaseAddress = new Uri(DataAccess.BaseLocation));
+            builder.Services.AddHttpClient<IJobCategoryDataService, JobCategoryDataService>(client => client.BaseAddress = new Uri(DataAccess.BaseLocation));
+
+
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
---
+++ GSBlazor/Services/DataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSBlazor.Services
+{
+    public class DataAccess
+    {
+        public static string BaseLocation = "https://localhost:44340"; //"http://localhost:5000/";
+    }
+}
